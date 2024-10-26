38caa1ee31801e4a63e6c91334d749174658bd55
Caleb Dowdy <x3caleb3x@gmail.com>
2024-10-26 14:51:14 -0400

    main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 #Function to List Cars
 def vehicles():
   print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles: ")
-  file = open("allowedVehicleList.txt", "r")
+  file = open("authorizedvehiclelist.txt", "r")
   response = file.read()
   print(response)
 #Function to Search for a Car
 def searchCar():
   car = input("Please Enter the full Vehicle Name: ")
-  with open("allowedVehicleList.txt", 'r') as search:
+  with open("authorizedvehiclelist.txt", 'r') as search:
     filing = search.read()
     if car in filing:
       print(str(car)+ " is an authorized vehicle")
@@ -28,9 +28,9 @@ def removeCar():
   deleteCar = input("Please Enter the full Vehicle name you would like to REMOVE: ")
   assurance = input("Are you sure you want to remove " + str(deleteCar) + " from the Authorized Vehicle List? ")
   if assurance == "yes":
-    with open("allowedVehicleList.txt", "r") as file:
+    with open("authorizedvehiclelist.txt", "r") as file:
       lines = file.readlines()
-    with open("allowedVehicleList.txt", "w") as file:
+    with open("authorizedvehiclelist.txt", "w") as file:
       for line in lines:
         if line.strip("\n") != deleteCar:
           file.write(line)
