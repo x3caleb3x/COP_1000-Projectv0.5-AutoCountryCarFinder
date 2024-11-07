03a2ad716a297543e18e7f122afec8edff7676cd
Caleb Dowdy <x3caleb3x@gmail.com>
2024-11-07 15:09:22 -0500

    Main.py

--- main.py
+++ main.py
@@ -23,6 +23,20 @@ def addCar():
     file.write("\n")
     file.write(newCar)
     print("You have added " + str(newCar) + " as an authorized vehicle")
+    
+#Function to Remove Car
+def removeCar():
+  deleteCar = input("Please Enter the full Vehicle name you would like to REMOVE: ")
+  assurance = input("Are you sure you want to remove " + str(deleteCar) + " from the Authorized Vehicle List? ")
+  if assurance == "yes":
+    with open("authorizedvehiclelist.txt", "r") as file:
+      lines = file.readlines()
+    with open("authorizedvehiclelist.txt", "w") as file:
+      for line in lines:
+        if line.strip("\n") != deleteCar:
+          file.write(line)
+    print("You have REMOVED " + str(deleteCar) + " as an authorized vehicle")
+
 
 #Menu
 while input != 5:
@@ -33,7 +47,8 @@ while input != 5:
   print("1. PRINT all Authorized Vehicles")
   print("2. SEARCH for Authorized Vehicle")
   print("3. ADD Authorized Vehicle")
-  print("4. Exit")
+  print("4. DELETE Authorized Vehicle")
+  print("5. Exit")
 
   menu = int(input(""))
   #Menu Selection
@@ -46,6 +61,12 @@ while input != 5:
   if menu == 3:
     addCar()
 
+  if menu == 4:
+   removeCar()
+
+  if menu == 5:
+    print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+    exit()
   if menu == 4:
     print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
     exit()
